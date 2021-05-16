119b408c147154ccbbf5ab6da78636c3603c30b2
Leonardo Saraiva <vyper@maneh.org>
2021-05-15 21:13:36 -0300

    feat(whatsapp): adds session persistence after successfull auth

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
+import * as fs from 'fs';
+import * as path from 'path';
 // eslint-disable-next-line node/no-extraneous-import
 import {Client} from 'whatsapp-web.js';
 import * as QRCode from 'qrcode';
 
-const client = new Client({});
+const SESSION_FILE_PATH = path.resolve(__dirname, 'session.json');
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
+const client = new Client({
+  session: sessionCfg,
+});
 
 client.on('qr', qr => {
   console.log('QR RECEIVED', qr);
   QRCode.toString(qr, {type: 'terminal'}, (_: Error, url: string) => {
     console.log(url);
+  });
+});
+
+client.on('authenticated', session => {
+  console.log('AUTHENTICATED', session);
+
+  sessionCfg = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), err => {
+    if (err) {
+      console.error(err);
+    }
   });
 });
 
